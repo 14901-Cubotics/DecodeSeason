bf4e138acd8d350cea92cf6d936083386149283e
BPetrie-MCA <bpetrie@mcacubs.org>
2025-11-12 06:40:00 -0600

    Create mechanumDrive.java
    Created the Mechanum Drive System.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/mechanumDrive.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.systems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class mechanumDrive {
+    private DcMotor leftFrontDrive;
+    private DcMotor leftRearDrive;
+    private DcMotor rightFrontDrive;
+    private DcMotor rightRearDrive;
+
+    public void init(HardwareMap hMap){
+        leftFrontDrive = hMap.get(DcMotor.class,"leftfront");
+        leftRearDrive = hMap.get(DcMotorEx.class,"leftrear");
+        rightFrontDrive = hMap.get(DcMotorEx.class,"rightfront");
+        rightRearDrive = hMap.get(DcMotorEx.class,"rightrear");
+
+        leftFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftRearDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightRearDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    public void driveRobot(double move, double strafe, double turn) {
+            // Calculate wheel powers.
+            double frontLeftPower    =  move - strafe - turn;
+            double frontRightPower   =  move + strafe + turn;
+            double backLeftPower     =  move + strafe - turn;
+            double backRightPower    =  move - strafe + turn;
+
+            // Normalize wheel powers to be less than 1.0
+            double max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+            max = Math.max(max, Math.abs(backLeftPower));
+            max = Math.max(max, Math.abs(backRightPower));
+
+            if (max > 1.0) {
+                frontLeftPower /= max;
+                frontRightPower /= max;
+                backLeftPower /= max;
+                backRightPower /= max;
+            }
+
+            // Send powers to the wheels.
+            leftFrontDrive.setPower(frontLeftPower);
+            rightFrontDrive.setPower(frontRightPower);
+            leftRearDrive.setPower(backLeftPower);
+            rightRearDrive.setPower(backRightPower);
+    }
+
+}
