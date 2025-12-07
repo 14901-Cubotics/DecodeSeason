74a2c8a7bf9875d627d108c15559709d2107621a
BPetrie-MCA <bpetrie@mcacubs.org>
2025-12-06 21:52:48 -0600

    - Added system classes for: -- Carousel -- intake - adjusted some code in mechanumDrive system - added a readme with device naming for easier setup of robot profile.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivemode2526.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivemode2526.java
@@ -1,27 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.teamcode.systems.mechanumDrive;
-import org.firstinspires.ftc.teamcode.systems.navSystem;
 
-@TeleOp(name="Drivemode 25-26", group="Competition")
+@TeleOp(name = "Drivemode 25-26", group = "Competition")
 public class drivemode2526 extends LinearOpMode {
-    //initialize systems
-    navSystem nav = new navSystem();
-    mechanumDrive mdrive = new mechanumDrive();
 
+    // Create any systems here, name and assign like the mechanuDrive
+    mechanumDrive mdrive = new mechanumDrive();
+    // intakeSystem
+    // carouselSystem
+    // launchSystem
 
     @Override
     public void runOpMode() throws InterruptedException {
+        // TODO: Essentially your main method
+
+       // Init any hardware here.
         mdrive.init(hardwareMap);
-        while (opModeIsActive()){
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Your loop, repeats as long as the game is active.
+
+            // variables for the drive numbers.
             double drive = -gamepad1.left_stick_y;
             double strafe = gamepad1.left_stick_x;
             double turn = gamepad1.right_stick_x;
 
+            //calling the mechanumDrive's drive method with our numbers.
+            mdrive.driveRobot(drive, strafe, turn);
         }
     }
 }
