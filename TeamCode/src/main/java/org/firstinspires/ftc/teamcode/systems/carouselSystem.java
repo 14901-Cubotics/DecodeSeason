1adab84c7e1210f4778994c3435b9e3a4363b53a
Lyssa <108826604+Lyssa0@users.noreply.github.com>
2026-01-20 16:47:54 -0600

    Updated carousel
    carousel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/carouselSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/carouselSystem.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.systems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class carouselSystem {
@@ -13,14 +14,17 @@ public class carouselSystem {
 
     public void init(HardwareMap hMap){
         carouselMotor = hMap.get(DcMotorEx.class,"carouselmotor");
-        carouselMotor.setDirection(DcMotor.Direction.FORWARD);
-        carouselMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        carouselMotor.setDirection(DcMotor.Direction.REVERSE);
+        carouselMotor.setTargetPositionTolerance(1);
     }
+    
 
     public void advanceCarousel (){
         currentTics = carouselMotor.getCurrentPosition();
         targetTics = currentTics + tpb;
         carouselMotor.setTargetPosition(targetTics);
+        carouselMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        carouselMotor.setPower(0.5);
     }
 
     // Create a public method that controls the carousel motor.
